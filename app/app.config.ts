970fc8baf45cd631473f9d749662d3185ff2d476
jack.wang <34840489@qq.com>
2025-10-26 19:50:11 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
-  title: 'Cn-url.cn',
+  title: '米粒科技-短链服务-Cn-url.cn',
   email: '34840489@qq.com',
   github: 'https://github.com/xyz007wjm/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
+  twitter: '#',
+  telegram: '#',
+  mastodon: '#',
+  blog: '#',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
