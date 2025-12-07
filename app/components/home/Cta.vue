85e435f25bb50669fa4ef2968e8f157e6e5ed27e
jack.wang <34840489@qq.com>
2025-12-07 18:07:43 +0800

    Remove CTA component template markup

--- app/components/home/Cta.vue
+++ app/components/home/Cta.vue
@@ -1,27 +1,3 @@
 <template>
-  <div
-    class="
-      mx-auto my-12 flex max-w-4xl flex-col items-center rounded-lg bg-black p-8
-      text-center
-      md:px-20 md:py-20
-    "
-  >
-    <h2
-      class="
-        text-4xl tracking-tight text-white
-        md:text-6xl
-      "
-    >
-      {{ $t('home.cta.title') }}
-    </h2>
-    <p
-      class="
-        mt-4 text-lg text-slate-400
-        md:text-xl
-      "
-    >
-      {{ $t('home.cta.description') }}
-    </p>
-   
-  </div>
+  
 </template>
